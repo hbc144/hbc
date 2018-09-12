675beaa66806b4336809c484f1754dac51047eaa
hbc144 <hbc144@qq.com>
2018-09-12 16:58:43 +0800

    提交

---
+++ homework1/homework1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            double a, b;
+            Console.Write("Please input a double:");
+            s = Console.ReadLine();
+            a = Double.Parse(s);
+            Console.Write("Please input another double:");
+            s = Console.ReadLine();
+            b = Double.Parse(s);
+            Console.Write(a + " * " + b + " = " + a * b);
+        }
+    }
+}
---
+++ homework1/homework1_2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            double a = Double.Parse(s1);
+            string s2 = textBox2.Text;
+            double b = Double.Parse(s2);
+            label1.Text=a+" * "+b+" = "+a*b;
+        }
+
+        
+    }
+}
